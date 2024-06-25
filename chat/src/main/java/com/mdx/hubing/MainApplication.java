61be5a1b482db6e1e12573df0083fe49eaf0fdef
meng <meng.liu1@bthome.com>
2024-06-25 17:52:29 +0800

    添加redis

--- chat/src/main/java/com/mdx/hubing/MainApplication.java
+++ chat/src/main/java/com/mdx/hubing/MainApplication.java
@@ -2,12 +2,14 @@ package com.mdx.hubing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Author: Meng
  * Date: 2023-03-09
  * Desc: 项目主入口
  */
+@EnableCaching
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
