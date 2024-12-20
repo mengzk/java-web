c6a261742150199880e4da8cf8e301110ca970a4
mdx <mdx@qq.com>
2024-12-20 21:52:01 +0800

    修改代码

--- src/main/java/com/mon/aichat/config/Configs.java
+++ src/main/java/com/mon/aichat/config/Configs.java
@@ -7,9 +7,9 @@ package com.mon.aichat.config;
  */
 public class Configs {
     // 智谱AI 配置
-    public final static String ZPSecret = "dV3gTIO7SasX8K6H";
-    public final static String ZPKey = "baaa696e5a564afe5e4ce49236e852b0";
-    public final static String RobotId = "baaa696e5a564afe5e4ce49236e852b0.dV3gTIO7SasX8K6H";
+    public final static String ZPSecret = "";
+    public final static String ZPKey = "";
+    public final static String RobotId = "";
 
     // WebSocket 配置
     public final static String WS_PATH = "link";
